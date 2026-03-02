70f3d799d239ec675f51879d25a2473c0a2d611f
Danny Ryan <dannykryan@gmail.com>
2026-03-02 22:09:43 +0000

    add student database 2 script

---
+++ student_database_2/students.sh
@@ -0,0 +1,36 @@
+students:
++------------+------------+--------------+----------+-----+
+| student_id | first_name |  last_name   | major_id | gpa |
++------------+------------+--------------+----------+-----+
+|          6 | Rhea       | Kellems      |       36 | 2.5 |
+|          7 | Emma       | Gilbert      |          |     |
+|          8 | Kimberly   | Whitley      |       37 | 3.8 |
+|          9 | Jimmy      | Felipe       |       36 | 3.7 |
+|         10 | Kyle       | Stimson      |          | 2.8 |
+|         11 | Casares    | Hijo         |       41 | 4.0 |
+|         12 | Noe        | Savage       |          | 3.6 |
+|         13 | Sterling   | Boss         |       41 | 3.9 |
+|         14 | Brian      | Davis        |          | 2.3 |
+|         15 | Kaija      | Uronen       |       41 | 3.7 |
+|         16 | Faye       | Conn         |       41 | 2.1 |
+|         17 | Efren      | Reilly       |       37 | 3.9 |
+|         18 | Danh       | Nhung        |          | 2.4 |
+|         19 | Maxine     | Hagenes      |       36 | 2.9 |
+|         20 | Larry      | Saunders     |       38 | 2.2 |
+|         21 | Karl       | Kuhar        |       37 |     |
+|         22 | Lieke      | Hazenveld    |       41 | 3.5 |
+|         23 | Obie       | Hilpert      |       37 |     |
+|         24 | Peter      | Booysen      |          | 2.9 |
+|         25 | Nathan     | Turner       |       36 | 3.3 |
+|         26 | Gerald     | Osiki        |       38 | 2.2 |
+|         27 | Vanya      | Hassanah     |       41 | 4.0 |
+|         28 | Roxelana   | Florescu     |       36 | 3.2 |
+|         29 | Helene     | Parker       |       38 | 3.4 |
+|         30 | Mariana    | Russel       |       37 | 1.8 |
+|         31 | Ajit       | Dhungel      |          | 3.0 |
+|         32 | Mehdi      | Vandenberghe |       36 | 1.9 |
+|         33 | Dejon      | Howell       |       37 | 4.0 |
+|         34 | Aliya      | Gulgowski    |       42 | 2.6 |
+|         35 | Ana        | Tupajic      |       38 | 3.1 |
+|         36 | Hugo       | Duran        |          | 3.8 |
++------------+------------+--------------+----------+-----+
